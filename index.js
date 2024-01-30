696209a81fea401bb832ccf332aa45e1420d91ba
chyntiaclaudia <chyntiaclaudia13@gmail.com>
2024-01-30 10:03:12 +0700

    cobain 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you cobain" }');
+    res.send('{ "response": "Hello From Thetips4you cobain lagi" }');
 });
 
 app.get('/will', function (req, res) {
